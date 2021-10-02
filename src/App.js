77c90c8b134bf049fc72c14fb3cca01a1a5d7f70
rai8 <hytonne@outlook.com>
2021-10-02 19:51:37 +0300

    simple counter reducer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer, useState } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//functions used by reducer
+const reducerFunction = (state, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { ...state, count: state.count + 1 }
+
+    case 'DECREMENT':
+      return { ...state, count: state.count - 1 }
+    case 'INITIALIZE_COUNT':
+      return { ...state, count: action.payload }
+
+    default:
+      return state
+  }
 }
 
-export default App;
+const initialState = {
+  count: 0,
+}
+
+function App() {
+  const [input, setInput] = useState(0)
+
+  //using useReducer
+  const [state, dispatch] = useReducer(reducerFunction, initialState)
+  return (
+    <div className='App'>
+      <p>GLOBAL STATE</p>
+      <div>
+        <label>Start Count</label>
+        <input
+          type='number'
+          value={input}
+          onChange={(e) => setInput(parseInt(e.target.value))}
+        />
+
+        <button
+          onClick={() => dispatch({ type: 'INITIALIZE_COUNT', payload: input })}
+        >
+          Initialize Count
+        </button>
+      </div>
+      <div style={{ display: 'flex' }}>
+        <button
+          style={{ marginRight: '10px' }}
+          onClick={() => dispatch({ type: 'DECREMENT' })}
+        >
+          Decrement
+        </button>
+        <p>{state.count}</p>
+        <button
+          style={{ marginLeft: '10px' }}
+          onClick={() => dispatch({ type: 'INCREMENT' })}
+        >
+          Increment
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
